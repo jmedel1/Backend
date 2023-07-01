ae5e4b7bc1bd8ea64c04d89dae19a315db4fc2dd
jael <jael.medel30@gmail.com>
2023-07-01 16:38:22 -0500

    update post model

---
+++ post.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  likes: { type: Number, default: 0 },
+  comments: [{ type: String }],
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
