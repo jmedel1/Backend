d0cd5cdbd37f72fa6825d40d9cbed00e460b77b6
jael <jael.medel30@gmail.com>
2023-07-01 22:29:20 -0500

    trying to fix backend

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# Backend
+# Backend Server
+
+This is the backend server for your chat application. It provides the necessary APIs to interact with the chat messages and perform actions such as adding messages, liking messages, and leaving comments.
+
+## Prerequisites
+
+Before running the backend server, make sure you have the following installed:
+
+- Node.js (version X.X.X or higher)
+- NPM (version X.X.X or higher)
+
+## Getting Started
+
+1. Clone this repository to your local machine.
+2. Navigate to the backend server directory.
+3. Install the dependencies by running the following command:
+
+   ```bash
+   npm install
+
+4. Start the backend server with the following command:
+
+    ```bash
+    npm start
+The server will start running on port 3000.
+
+## API Endpoints
+The backend server exposes the following API endpoints:
+
+- GET /api/messages: Retrieve all chat messages.
+- POST /api/messages: Add a new chat message.
+- PUT /api/messages/:id/like: Like a chat message.
+- PUT /api/messages/:id/comment: Add a comment to a chat message.
+
+## Database
+The backend server uses a JSON file (db.json) as the database to store the chat messages. The file is located in the root directory of the server.
+
+## Contributing
+If you'd like to contribute to the development of this backend server, you can follow these steps:
+
+Fork the repository and clone it to your local machine.
+Create a new branch for your feature or bug fix.
+Implement your changes and test them thoroughly.
+Commit and push your changes to your forked repository.
+Submit a pull request with a detailed description of your changes.
+
+## License
+This project is licensed under the MIT License.
--- server.js
+++ server.js
@@ -1,13 +1,107 @@
 const express = require('express');
-const app = express();
-const routes = require('./routes');
+const cors = require('cors');
+const fs = require('fs');
 
-// Set up middleware, routes, and other configurations
-app.use(express.json()); // Parse JSON bodies
-app.use(routes); // Add routes
+const app = express();
+app.use(cors()); // Enable CORS
+
+// Rest of your code...
+
+
+// Middleware
+app.use(express.json());
+
+// Helper function to read data from JSON file
+const readDataFromJsonFile = () => {
+  try {
+    const jsonData = fs.readFileSync('db.json', 'utf8');
+    return JSON.parse(jsonData);
+  } catch (error) {
+    console.error('Error reading the JSON file:', error);
+    return [];
+  }
+};
+
+// Helper function to write data to JSON file
+const writeDataToJsonFile = (data) => {
+  try {
+    fs.writeFileSync('db.json', JSON.stringify(data), 'utf8');
+  } catch (error) {
+    console.error('Error writing to the JSON file:', error);
+  }
+};
+
+// Routes
+app.get('/api/posts/top-liked', (req, res) => {
+  try {
+    const posts = readDataFromJsonFile();
+    const sortedPosts = posts.sort((a, b) => b.likes - a.likes);
+    const topLikedPosts = sortedPosts.slice(0, 10); // Get top 10 liked posts
+    res.json(topLikedPosts);
+  } catch (error) {
+    console.error('Error fetching top liked posts:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/api/posts/top-commented', (req, res) => {
+  try {
+    const posts = readDataFromJsonFile();
+    const sortedPosts = posts.sort((a, b) => b.comments.length - a.comments.length);
+    const topCommentedPosts = sortedPosts.slice(0, 10); // Get top 10 commented posts
+    res.json(topCommentedPosts);
+  } catch (error) {
+    console.error('Error fetching top commented posts:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.put('/api/posts/:id/like', (req, res) => {
+  try {
+    const postId = req.params.id;
+    const posts = readDataFromJsonFile();
+    const post = posts.find((p) => p._id === postId);
+
+    if (!post) {
+      return res.status(404).json({ error: 'Post not found' });
+    }
+
+    // Update the post's like count
+    post.likes += 1;
+
+    writeDataToJsonFile(posts);
+
+    res.json(post);
+  } catch (error) {
+    console.error('Error liking the post:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.put('/api/posts/:id/comment', (req, res) => {
+  try {
+    const postId = req.params.id;
+    const posts = readDataFromJsonFile();
+    const post = posts.find((p) => p._id === postId);
+
+    if (!post) {
+      return res.status(404).json({ error: 'Post not found' });
+    }
+
+    // Update the post's comment count
+    post.comments.push({ text: req.body.text });
+
+    writeDataToJsonFile(posts);
+
+    res.json(post);
+  } catch (error) {
+    console.error('Error commenting on the post:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = 3000; // Use any port number you prefer
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
